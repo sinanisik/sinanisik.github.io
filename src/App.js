bc09c1a617a5eb2b4578543ff650e7fc08b4eba4
Sinan Işık <sinan.isik@ug.bilkent.edu.tr>
2020-06-15 23:48:23 +0300

    push portfolio code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Sidebar from "./Components/Sidebar";
+import Landing from "./Components/Landing";
+import profileData from "./profileData.json";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      landingData: profileData.landing,
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Sidebar sidebarData={this.state.landingData} />
+        <div className="container-fluid p-0">
+          <Landing landingData={this.state.landingData} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
