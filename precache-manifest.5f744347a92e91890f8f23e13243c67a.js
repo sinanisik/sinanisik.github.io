e9e06367758dcf5a5abc70e44de2f2ef9a3f3f57
Sinan Işık <sinan.isik@ug.bilkent.edu.tr>
2020-06-16 10:35:35 +0300

    Updates

---
+++ precache-manifest.5f744347a92e91890f8f23e13243c67a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4049bacde19d51b26c80fc7ebb97865",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a0c435464196cda2874",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fd811868def9a02b54fd",
+    "url": "/static/js/2.1ee8b966.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1ee8b966.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a0c435464196cda2874",
+    "url": "/static/js/main.94004bab.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "f99f338317523ba422c0d30a1762ed47",
+    "url": "/static/media/profile.f99f3383.jpg"
+  }
+]);
